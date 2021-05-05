f39deb814b492423245891019f908e8f9c73aaf8
Vishal7017 <wavhalev@gmail.com>
2021-05-06 01:26:26 +0530

    Saveme

--- app/run.py
+++ app/run.py
@@ -1,149 +1,122 @@
 import json
 import plotly
 import pandas as pd
-
-import re
+import joblib
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
 def tokenize(text):
-    '''
-    INPUT - text (string)
-    OUTPUT - tokenized and cleansed text (string)
-
-    This function tokenizes and cleanses a string by the following steps:
-        1. any url will be replaced by the string 'urlplaceholder'
-        2. any puctuation and capitalization will be removed
-        3. the text will be tokenized
-        4. any stopword will be removed
-        5. each word will be first lemmatized and then stemmed
-    '''
-
-    # get list of all urls using regex
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    detected_urls = re.findall(url_regex, text)
-
-    # replace each url in text string with placeholder
-    for url in detected_urls:
-        text = text.replace(url, 'urlplaceholder')
-
-    # Remove punctuation characters
-    text = re.sub(r'[^a-zA-Z0-9äöüÄÖÜß ]', '', text.lower())
-
-    # tokenize
     tokens = word_tokenize(text)
-
-    # Remove stop words
-    tokens = [tok for tok in tokens if tok not in stopwords.words("english")]
-
-    # instantiate lemmatizer and stemmer
     lemmatizer = WordNetLemmatizer()
-    stemmer = PorterStemmer()
 
-    # lemmatize and stemm
     clean_tokens = []
     for tok in tokens:
-        lemm_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tok = stemmer.stem(tok)
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
 
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('categorized_messages', engine)
+engine = create_engine('sqlite:///../data/DisasterResponsecopy.db')
+df = pd.read_sql_table('Messages', engine)
 
 # load model
-model = joblib.load("classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
+
 def index():
-
+    
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-
-    #category top 10 prep
-    cat_cnt = df.drop(columns=['id','message','original','genre','related']).sum(axis=0).sort_values(ascending=False)
-    cat_top = cat_cnt.head(10)
-    cat_lab = list(cat_top.index)
+    # Calculate message count by genre and related status    
+    genre_related = df[df['related']==1].groupby('genre').count()['message']
+    genre_not_rel = df[df['related']==0].groupby('genre').count()['message']
+    genre_names = list(genre_related.index)
+    
+    # Calculate proportion of each category with label = 1
+    cat_props = df.drop(['id', 'message', 'original', 'genre'], axis = 1).sum()/len(df)
+    cat_props = cat_props.sort_values(ascending = False)
+    cat_names = list(cat_props.index)
+     
 
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
                 Bar(
                     x=genre_names,
-                    y=genre_counts
+                    y=genre_related,
+                    name = 'Related'
+                ),
+                
+                Bar(
+                    x=genre_names,
+                    y=genre_not_rel,
+                    name = 'Not Related'
                 )
             ],
 
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Distribution of Messages by Genre and Related Status',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
                     'title': "Genre"
-                }
+                },
+                'barmode': 'group'
             }
         },
         {
             'data': [
                 Bar(
-                    x=cat_lab,
-                    y=cat_top
+                    x=cat_names,
+                    y=cat_props
                 )
             ],
 
             'layout': {
-                'title': 'Top 10 Message Categories',
+                'title': 'Proportion of Messages by Category',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Proportion"
                 },
                 'xaxis': {
-                    'title': "Category"
+                    'title': "Category",
+                    'tickangle': -45
                 }
             }
         }
     ]
-
+    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-
+    
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '')
+    query = request.args.get('query', '') 
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file.
+    # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
